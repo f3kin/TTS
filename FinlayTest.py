527fdaa7a969b01418566bb1feff0208aacfcbd3
f3kin <60875fil@gmail.com>
2024-12-20 12:51:41 +1100

    Created and Tested

---
+++ FinlayTest.py
@@ -0,0 +1,36 @@
+import torch
+from TTS.api import TTS
+import pygame
+
+def play(filename):
+    pygame.mixer.quit()
+    pygame.mixer.init()
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.stop()
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+    pygame.mixer.music.stop()
+    pygame.mixer.quit()
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models
+print(TTS().list_models())
+
+# Init TTS
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+# Run TTS
+# ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+# Text to speech list of amplitude values as output
+# wav = tts.tts(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en")
+# Text to speech to a file
+tts.tts_to_file(text="Joe Biden is way too skibidi!", speaker_wav="trump.wav", language="en", file_path="output.wav")
+
+play('output.wav')
+
+
